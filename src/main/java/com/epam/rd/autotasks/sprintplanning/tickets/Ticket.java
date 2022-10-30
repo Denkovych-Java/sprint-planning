f8d5d6747949e5181df4d55c4ad0f9f8542bbe7c
Oleg <74234001+Denkovych-Java@users.noreply.github.com>
2022-10-30 19:26:36 +0200

    Update Ticket.java

--- src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/Ticket.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/Ticket.java
@@ -1,15 +1,5 @@
 package com.epam.rd.autotasks.sprintplanning.tickets;
 
-/*Ticket
- аждый ticket имеет свой идентификатор, им€ и примерное врем€ дл€ его выполнени€. Ёти значени€ предоставл€ютс€ через конструктор класса Ticket.
-Ticket может считатьс€ завершенным или незавершенным. “олько что созданный ticket считаетс€ незавершенным.
-getId() - возвращает идентификатор ticketТа.
-getName() - возвращает им€ ticketТа.
-getEstimate() - возвращает оценку ticketТа.
-isCompleted() - возвращает true, если ticket завершен, в противном случае Ч false.
-complete() - переводит ticket в завершенное состо€ние.
-
- */
 public class Ticket {
     private final int id;
     private final String name;
